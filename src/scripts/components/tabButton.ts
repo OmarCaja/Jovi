40660835ef9ba8ecd641f79cafc86fdd8a90bc3e
Omar Caja <omar.caja@hotmail.com>
2023-05-15 12:22:59 +0200

    Added esLint

--- src/scripts/components/tabButton.ts
+++ src/scripts/components/tabButton.ts
@@ -1,25 +1,21 @@
-const buttons: NodeListOf<HTMLElement> = document.querySelectorAll(".js-tab-button");
-let displayedSectionId: string;
-
-let test;
+const buttons: NodeListOf<HTMLElement> = document.querySelectorAll('.js-tab-button')
+let displayedSectionId: string
 
 buttons.forEach(button => {
-	button.addEventListener('click', () => bindTab(button));
+  button.addEventListener('click', () => { bindTab(button) })
 })
-buttons.item(0).click();
+buttons.item(0).click()
 
-function bindTab(button: HTMLElement): void {
+function bindTab (button: HTMLElement): void {
+  const displayedSection: HTMLElement = document.getElementById(displayedSectionId)
 
-	const displayedSection: HTMLElement = document.getElementById(displayedSectionId);
+  if (displayedSection == null) {
+    displayedSection.style.display = 'none'
+  }
 
-	if (displayedSection) {
-		displayedSection.style.display = 'none';
-	}
+  const id: string = button.dataset.targetId
+  displayedSectionId = id
 
-	const id: string = button.dataset.targetId;
-	displayedSectionId = id;
-
-	const section: HTMLElement = document.getElementById(id);
-	section.style.display = 'flex';
+  const section: HTMLElement = document.getElementById(id)
+  section.style.display = 'flex'
 }
-
